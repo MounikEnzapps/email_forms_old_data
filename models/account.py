a42c04adea469a145479babc0580ec8e489095af
USER <yarmiztechnlogies@gmail.com>
2022-10-18 17:56:48 +0530

    <MounikEnzapps>

---
+++ models/account.py
@@ -0,0 +1,59 @@
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+from odoo.tools.misc import formatLang, format_date, get_lang
+
+from odoo import _, api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    # def _post(self, soft=True):
+    #     if not self.invoice_date:
+    #         if self.invoice_date_time:
+    #             self.invoice_date_time.split('/')
+    #             year = self.invoice_date_time.split('/')[2]
+    #             month = self.invoice_date_time.split('/')[1]
+    #             dates = self.invoice_date_time.split('/')[0]
+    #             self.invoice_date=year+'-'+month+'-'+dates
+    #
+    #     if self.einv_sa_delivery_date < self.invoice_date:
+    #         self.einv_sa_delivery_date =  self.invoice_date
+    #     res = super()._post(soft)
+    #     for record in self:
+    #         if record.country_code == 'SA' and record.move_type in ('out_invoice', 'out_refund'):
+    #             if not record.einv_sa_show_delivery_date:
+    #                 raise UserError(_('Delivery Date cannot be empty'))
+    #             if record.einv_sa_delivery_date < record.invoice_date:
+    #
+    #                 raise UserError(_('Delivery Date cannot be before Invoice Date'))
+    #             self.write({
+    #                 'einv_sa_confirmation_datetime': fields.Datetime.now()
+    #             })
+    #     return res
+
+
+
+class AutomaticNatcomRecord(models.Model):
+    _inherit = 'automatic.natcom.record'
+
+    def auto_confirm_all(self):
+        for each_inv in self.op_lines:
+            if each_inv.invoice_id.state == "draft":
+                if not each_inv.invoice_id.invoice_date:
+                    if each_inv.invoice_id.invoice_date_time:
+                                each_inv.invoice_id.invoice_date_time.split('/')
+                                year = each_inv.invoice_id.invoice_date_time.split('/')[2]
+                                month = each_inv.invoice_id.invoice_date_time.split('/')[1]
+                                dates = each_inv.invoice_id.invoice_date_time.split('/')[0]
+                                each_inv.invoice_id.invoice_date=year+'-'+month+'-'+dates
+
+                    if each_inv.invoice_id.einv_sa_delivery_date < each_inv.invoice_id.invoice_date:
+                        each_inv.invoice_id.einv_sa_delivery_date =  each_inv.invoice_id.invoice_date
+
+                each_inv.invoice_id.action_post()
+                each_inv.write({'state': 'posted'})
+        self.write({'state':'close'})
+
+
+
